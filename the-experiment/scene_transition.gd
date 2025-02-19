e3015dca1d50df305623848c895947b847fbe158
Jeremiah Knizley <TheMainMoe@gmail.com>
2025-02-18 23:30:08 -0500

    Fixed scene transition. Added dialogue manager

--- the-experiment/scene_transition.gd
+++ the-experiment/scene_transition.gd
@@ -1,20 +1,20 @@
 extends CanvasLayer
 
 # List of scenes to switch between
-var scenes = ["res://Scene1.tscn", "res://Scene2.tscn", "res://Scene3.tscn"]
+var scenes = [preload("res://Scenes/scene1.tscn"), preload("res://Scenes/scene2.tscn"), preload("res://Scenes/scene3.tscn")]
 var current_scene_index = 0
 
 func _ready() -> void:
 	# Load the first scene
-	change_scene(scenes[current_scene_index])
+	print("scene_transition ready")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_select"):  # "ui_select" is mapped to the spacebar by default
 		current_scene_index = (current_scene_index + 1) % scenes.size()
 		change_scene(scenes[current_scene_index])
 
-func change_scene(scene_path: String) -> void:
-	$AnimationPlayer.play('dissolve')
-	await $AnimationPlayer.animation_finished
-	get_tree().change_scene(scene_path)
+func change_scene(scene_path: Resource) -> void:
+	$dissolve_rect/AnimationPlayer.play('dissolve')
+	await $dissolve_rect/AnimationPlayer.animation_finished
+	get_tree().change_scene_to_packed(scene_path)
 	$dissolve_rect/AnimationPlayer.play_backwards('dissolve')
