388f5e01d469e7e8365bdcd9c1bb2b890509f02f
Jeremiah Knizley <TheMainMoe@gmail.com>
2025-02-23 14:24:20 -0500

    Remodeled folder structure, added all sample scenes, scripts, dialogue. Added some globals and utilities.

---
+++ the-experiment/Scripts/Globals/state.gd
@@ -0,0 +1,20 @@
+extends Node
+
+signal change_texture
+signal speaker_changed(speaker: String)
+
+var scene_changer = preload("res://Scenes/Utilities/scene_transition.tscn")
+
+@onready var scenes = {
+	"Alchemy": preload("res://Scenes/Levels/Alchemy.tscn"),
+	"Armory": preload("res://Scenes/Levels/Armory.tscn"),
+	"Dungeon": preload("res://Scenes/Levels/Dungeon.tscn"),
+	"Garden": preload("res://Scenes/Levels/Garden.tscn"),
+	"Hallway": preload("res://Scenes/Levels/Hallway.tscn"),
+	"Lab": preload("res://Scenes/Levels/Lab.tscn"),
+	"Library": preload("res://Scenes/Levels/Library.tscn"),
+	}
+
+#IF REFERENCING FROM DIALOGUE, NEXT LINE MUST BE => END
+func change_current_scene(scene: String):
+	get_tree().change_scene_to_packed(scenes[scene])
